cfc10a9bad22d466895bc261ff4644e767dd1171
gamesketches <sam.von.ehren@gmail.com>
2020-12-12 15:18:36 -0500

    first commit

---
+++ Assets/SCRIPTS/SnowballObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballObject : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public List<GameObject> snowHitAlready = new List<GameObject>();
+
+    private void OnParticleCollision(GameObject other)
+    {
+            var magnitude = 400;
+            var force = transform.position - other.transform.position;
+            force.Normalize();
+            rb.AddForce(force * magnitude);
+      
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SCRIPTS/SnowcloudObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowcloudObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector2.right * Time.deltaTime * 1f);
+    }
+}

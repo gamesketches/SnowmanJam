75800c2f223fe9542b52f644baa5cd711591bd74
gamesketches <sam.von.ehren@gmail.com>
2020-12-18 00:10:29 -0500

    Adding in dynamic level select

--- Assets/SCRIPTS/TitleScreenBehavior.cs
+++ Assets/SCRIPTS/TitleScreenBehavior.cs
@@ -2,21 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreenBehavior : MonoBehaviour
 {
+    public GameObject canvas;
+    public GameObject buttonPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(SceneManager.sceneCountInBuildSettings);
+        for(int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            Debug.Log(i);
+            GameObject levelButton = Instantiate<GameObject>(buttonPrefab, canvas.transform);
+            levelButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(-150 + (50 * i), 0);
+            string path = SceneUtility.GetScenePathByBuildIndex(i);
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(path);
+            levelButton.GetComponentInChildren<Text>().text = sceneName;
+            levelButton.GetComponent<Button>().onClick.AddListener(() => this.ChangeScene(path));
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            SceneManager.LoadScene(1);
-        }
+    }
+
+    void ChangeScene(string sceneName)
+    {
+        Debug.Log(sceneName);
+        SceneManager.LoadScene(sceneName);
+
     }
 }
